4176c5c7c33aeab4ec07c35d689401ca98053506
Sebastian Lopez <sebastianlopez83@hotmail.com>
2023-05-02 22:26:59 -0300

    02-05-23.v2

---
+++ G7_EE06/src/g7_ee06/G7_EE06.java
@@ -0,0 +1,21 @@
+package g7_ee06;
+
+import Entidad.Rectangulo;
+
+
+
+public class G7_EE06 {
+
+    public static void main(String[] args) {
+        
+        Rectangulo rectangulo1 = new Rectangulo();
+
+        rectangulo1.crear_rectangulo();
+        System.out.println("");
+        rectangulo1.calcular_area();
+        System.out.println("");
+        
+      
+    }
+
+}
